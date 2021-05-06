e5bca4695a5a261fee5cd66cedec77b829ec0846
torbas <t.orbasido@gmail.com>
2021-05-06 02:07:48 -0700

    rename files and set up employees route

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\EmployeeRepository;
+
+class EmployeeController extends Controller
+{
+    protected $empRepository;
+
+    public function __construct(EmployeeRepository $empRepository)
+    {
+        $this->empRepository = $empRepository;
+    }
+
+    public function index()
+    {
+        $employees = $this->empRepository->listAll();
+        return view('employees.list',['employees'=>$employees]);
+    }
+}
